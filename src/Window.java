c684f131d776162c5a75992f932562fe94d3a4d4
Shai Givati <shai@gmail.com>
2023-06-17 15:01:59 +0300

    Initial commit

---
+++ src/Window.java
@@ -0,0 +1,16 @@
+
+import javax.swing.*;
+
+import static ash.college.utils.Constants.WINDOW_HEIGHT;
+import static ash.college.utils.Constants.WINDOW_WIDTH;
+
+public class Window extends JFrame {
+    public static void main(String[] args) {
+        new Window();
+    }
+
+    public Window() {
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setVisible(true);
+    }
+}
